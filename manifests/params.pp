ea69a3edeef1e6a8c3a73c5a1cecdb2a52f0aa29
Michael G. Noll <mnoll@verisign.com>
2013-10-01 10:13:16 +0200

    Simplify params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,38 +15,25 @@ class graphite::params {
 
   case $::operatingsystem {
     'CentOS', 'Fedora', 'RedHat', 'Amazon' ,'Scientific': {
-      # main application
       $package_carbon  = [ 'python-carbon' ]
       $package_whisper = [ 'python-whisper' ]
       $package_web     = [ 'graphite-web']
-    }
-    default: {
-      fail("'${module_name}' provides no package default value for '${::operatingsystem}'")
-    }
-  }
-
-  case $::operatingsystem {
-    'CentOS', 'Fedora', 'RedHat', 'Amazon', 'Scientific': {
       $service_default_path     = '/etc/sysconfig'
-
       $service_cache_name       = 'carbon-cache'
       $service_cache_hasrestart = true
       $service_cache_hasstatus  = true
       $service_cache_pattern    = $service_cache_name
-
       $service_relay_name       = 'carbon-relay'
       $service_relay_hasrestart = true
       $service_relay_hasstatus  = true
       $service_relay_pattern    = $service_relay_name
-
       $service_aggregator_name       = 'carbon-aggregator'
       $service_aggregator_hasrestart = true
       $service_aggregator_hasstatus  = true
       $service_aggregator_pattern    = $service_aggregator_name
-
     }
     default: {
-      fail("'${module_name}' provides no service parameters for '${::operatingsystem}'")
+      fail("'${module_name}' does not support operating system '${::operatingsystem}'")
     }
   }
 
