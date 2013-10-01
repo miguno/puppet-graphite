f6f839d2555ff05d106dc2cac82df3c057e2adfa
Michael G. Noll <mnoll@verisign.com>
2013-10-01 11:15:49 +0200

    Remove incorrect doc header, not fully matching code

--- manifests/web/config.pp
+++ manifests/web/config.pp
@@ -1,27 +1,3 @@
-# == Class: graphite::web::config
-#
-# This class exists to coordinate all configuration related actions,
-# functionality and logical units in a central place.
-#
-#
-# === Parameters
-#
-# This class does not provide any parameters.
-#
-#
-# === Examples
-#
-# This class may be imported by other classes to use its functionality:
-#   class { 'graphite::carbon::config': }
-#
-# It is not intended to be used directly by external resources like node
-# definitions or other modules.
-#
-#
-# === Authors
-#
-# * Richard Pijnenburg <mailto:richard@ispavailability.com>
-#
 class graphite::web::config {
 
   #### Configuration
