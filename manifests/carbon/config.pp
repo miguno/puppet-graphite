6090ca482337f0a5daf5c755bdc098c1125f8842
Michael G. Noll <mnoll@verisign.com>
2013-10-16 15:34:49 +0200

    Refactor code

--- manifests/carbon/config.pp
+++ manifests/carbon/config.pp
@@ -1,5 +1,7 @@
 class graphite::carbon::config {
 
+  ### General configuration
+
   file { '/etc/carbon':
     ensure => directory,
     owner  => 'root',
@@ -16,6 +18,18 @@ class graphite::carbon::config {
     require => File['/etc/carbon'],
   }
 
+  ### carbon-aggregator
+
+  file { '/etc/carbon/aggregation-rules.conf':
+    ensure  => file,
+    content => template("$graphite::carbon::aggregator_config_template"),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => File['/etc/carbon'],
+  }
+
+  ### carbon-cache
 
   file_fragment { "header_${::fqdn}":
     tag     => "carbon_cache_storage_config_${::fqdn}",
@@ -25,15 +39,6 @@ class graphite::carbon::config {
 
   file_concat { '/etc/carbon/storage-schemas.conf':
     tag     => "carbon_cache_storage_config_${::fqdn}",
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => File['/etc/carbon'],
-  }
-
-  file { '/etc/carbon/aggregation-rules.conf':
-    ensure  => file,
-    content => template("$graphite::carbon::aggregator_config_template"),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
